5cdfd0d134c3f6d6558b8a9357e6a8235436a4ed
Justin Tamblyn <just1689@gmail.com>
2023-05-02 16:45:58 +0200

    added basic web app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ultimate-mafia
+
---
+++ client/web/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HomeComponent} from "../components/home/home.component";
+import { GameComponent } from '../components/game/game.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    GameComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
